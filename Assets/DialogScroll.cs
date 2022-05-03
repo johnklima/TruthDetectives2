e49cecebf4715e27b96b7ca13ae735f847028898
johnklima <klima.de.sa@gmail.com>
2022-05-03 11:04:05 +0100

    gui progress

---
+++ Assets/DialogScroll.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogScroll : MonoBehaviour
+{
+
+    public float timer = -1;    
+    public Transform child;
+
+    private void Awake()
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = Time.time;
+        child = transform.GetChild(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time - timer > 0.5f && timer > 0)
+        {
+            
+            child.gameObject.SetActive(true);
+            if(child.childCount > 0)
+            { 
+                child = child.GetChild(0);
+                timer = Time.time;
+
+            }
+            else 
+            {
+                timer = -1;
+            }    
+    
+        }
+    }
+}
